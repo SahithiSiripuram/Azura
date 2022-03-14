5b0269578feb982a8dc93b7c43a31a6fa54c2cef
Sahithi Siripuram <sahithi.s@comakeit.com>
2022-03-14 13:12:55 +0530

    countdown

--- pages/countdown.jsx
+++ pages/countdown.jsx
@@ -1,11 +1,38 @@
-import Link from 'next/link'
+import { useReducer } from 'react';
 import styles from '../styles/Home.module.css'
+const CardStyle = {
+    border: "1px solid #1A74E2",
+    padding: "20px",
+    margin: "20px",
+    width: "200px",
+  };
 function Countdown() {
-    return(
-        <div className={styles.container}>
-            <div className={styles.card}>
-            <p>Countdown timer goes here</p>
+    const [, forceUpdate] = useReducer(x => x + 1, 0);
+    var countDownDate = new Date("Mar 26, 2022 9:00:00").getTime();
+    var myfunc = setInterval(function () {
+        forceUpdate();
+    }, 60*1000);
+    var now = new Date().getTime();
+    var timeleft = countDownDate - now;
+    var days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    return (
+        <div  className={styles.container}>
+        <div className={styles.grid}>
+            <div style={CardStyle} className={styles.cardTimer}>
+                <p className={styles.headingTimer}>{days}</p>
+                <p className={styles.description}>days</p>
             </div>
+            <div style={CardStyle} className={styles.cardTimer}>
+                <p className={styles.headingTimer}>{hours}</p>
+                <p className={styles.description}>hours</p>
+            </div>
+            <div style={CardStyle} className={styles.cardTimer}>
+                <p className={styles.headingTimer}>{minutes}</p>
+                <p className={styles.description}>minutes</p>
+            </div>
+        </div>
         </div>
     )
 }
--- pages/team.jsx
+++ pages/team.jsx
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import {FaPhoneAlt} from "react-icons/fa"
