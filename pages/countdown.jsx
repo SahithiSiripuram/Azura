7f744b4ee80f4193731adfc260a817df504f5b9a
Sahithi Siripuram <sahithi.s@comakeit.com>
2022-03-15 11:15:35 +0530

    css fixes

--- pages/countdown.jsx
+++ pages/countdown.jsx
@@ -1,11 +1,6 @@
 import { useReducer } from 'react';
 import styles from '../styles/Home.module.css'
-const CardStyle = {
-    border: "1px solid #1A74E2",
-    padding: "20px",
-    margin: "20px",
-    width: "200px",
-  };
+
 function Countdown() {
     const [, forceUpdate] = useReducer(x => x + 1, 0);
     var countDownDate = new Date("Mar 26, 2022 9:00:00").getTime();
@@ -20,24 +15,26 @@ function Countdown() {
     var seconds = Math.floor((timeleft) % (1000 * 60) / (1000));
     return (
         <div  className={styles.container}>
-        <div className={styles.grid}>
-            <div style={CardStyle} className={styles.cardTimer}>
-                <p className={styles.headingTimer}>{days}</p>
-                <p className={styles.description}>days</p>
+            <div className={styles.main}>
+                <div className={styles.grid}>
+                    <div className={styles.cardTimer}>
+                        <p className={styles.headingTimer}>{days}</p>
+                        <p className={styles.description}>days</p>
+                    </div>
+                    <div className={styles.cardTimer}>
+                        <p className={styles.headingTimer}>{hours}</p>
+                        <p className={styles.description}>hours</p>
+                    </div>
+                    <div className={styles.cardTimer}>
+                        <p className={styles.headingTimer}>{minutes}</p>
+                        <p className={styles.description}>minutes</p>
+                    </div>
+                    <div className={styles.cardTimer}>
+                        <p className={styles.headingTimer}>{seconds}</p>
+                        <p className={styles.description}>seconds</p>
+                    </div>
+                </div>
             </div>
-            <div style={CardStyle} className={styles.cardTimer}>
-                <p className={styles.headingTimer}>{hours}</p>
-                <p className={styles.description}>hours</p>
-            </div>
-            <div style={CardStyle} className={styles.cardTimer}>
-                <p className={styles.headingTimer}>{minutes}</p>
-                <p className={styles.description}>minutes</p>
-            </div>
-            <div style={CardStyle} className={styles.cardTimer}>
-                <p className={styles.headingTimer}>{seconds}</p>
-                <p className={styles.description}>seconds</p>
-            </div>
-        </div>
         </div>
     )
 }
