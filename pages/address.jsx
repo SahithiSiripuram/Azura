0003ed87a7397938d4356580fc46a20ea4d7abe0
Sahithi Siripuram <sahithi.s@comakeit.com>
2022-03-15 12:05:56 +0530

    maps

--- pages/address.jsx
+++ pages/address.jsx
@@ -1,31 +1,14 @@
 import React from 'react'
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
-import { useJsApiLoader } from '@react-google-maps/api';
-
-const containerStyle = {
-  width: '400px',
-  height: '400px'
-};
-
-const center = {
-  lat: -3.745,
-  lng: -38.523
-};
-
 function Address() {
   return (
-    <LoadScript
-      googleMapsApiKey="YOUR_API_KEY"
-    >
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-    </LoadScript>
+    <iframe 
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7606.06359007908!2d78.48353507462812!3d17.601225001803122!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bcb850bb545e95b%3A0x4367e509f5ff38e5!2sCMR%20College%20of%20Engineering%20%26%20Technology%2C%20Hyderabad%20(CMRCET%2FCMRK)!5e0!3m2!1sen!2sin!4v1647325804948!5m2!1sen!2sin" 
+      width="600" 
+      height="450" 
+      style={{border:0}} 
+      allowFullScreen="" 
+      loading="lazy"
+    />
   )
 }
 
