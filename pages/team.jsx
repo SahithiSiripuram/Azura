b4eecd3788d905299be91ae9ddac85cee13f8c88
Sahithi Siripuram <sahithisiripuram1001@gmail.com>
2023-03-09 14:44:57 +0530

    Update team.jsx

--- pages/team.jsx
+++ pages/team.jsx
@@ -9,7 +9,7 @@ const Team = () => {
       <div className={styles.container}>
         <Container>
           <Row className={styles.title}>
-            <h1><span>Student Coordinators</span> for Azura 2k22</h1>
+            <h1><span>Student Coordinators</span> for Azura 2k23</h1>
           </Row>
           <Row xs={1} sm={1} md={2}>
           {
